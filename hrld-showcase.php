054af7a49646c4dcceda9071cf0af39d13b9c23c
jasonjan2003 <jasonjan2003@gmail.com>
2015-02-04 00:35:40 -0600

    initial development

---
+++ hrld-showcase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: hrld-showcase
+ * Plugin URI: http://badgerherald.com
+ * Description: This is a wordpress plugin to support a lightbox-style display of all media attachment on a single page and author page
+ * Version: 0.1
+ * Author: Jason Chan
+ * Author URI: http://badgerherald.com
+ * Network: 
+ * License: GPL2
+ */
+
+
+?>
