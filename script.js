86f798191a025ed78f922c6ead25d903d2907528
Jason Jan <jasonjan2003@gmail.com>
2015-02-15 20:54:25 -0600

    lots of functions added. fetches full size photos now.

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ jQuery(document).ready(function(){
 	//variables after click
 	var clicked_id;
 
-	// we need to label the images that
+	// build gallery backbone
+	hrld_showcase_build_gallery( jQuery('div#content'));
+
+	// we need to label and get the urls of the images that
 	// will be put into showcase.
 	imgs_length = hrld_showcase_init_label(imgs);
 
@@ -23,7 +26,31 @@ jQuery(document).ready(function(){
 		clicked_id = hrld_showcase_find_clicked_id(jQuery(this));
 
 		// build gallery
+		hrld_showcase_add_imgs(clicked_id);
 
+		// show gallery and first one in series
+		jQuery('li.showcase-gallery-img-1').show();
+		jQuery('div.showcase-gallery').fadeIn(800);
+
+		// nav
+		jQuery('.showcase-previous').click(function(event) {
+			if(clicked_id == 1)
+				jQuery('.gallery-close-wrapper').click();
+			else{
+				
+			}
+		});
+
+		// close
+		jQuery('imgwrapper, .gallery-close-wrapper').click(function(event) {
+			jQuery('.showcase-gallery').fadeOut(600);
+			jQuery('.showcase-gallery imgwrapper, .showcase-gallery nav').remove();
+			return;
+		});
+		jQuery('.showcase-gallery imgwrapper imgcontainer img').click(function(e){
+			e.stopPropagation();
+		})
+		return;
 	});
 	
 });
@@ -53,13 +80,12 @@ function hrld_showcase_init_label( imgs){
 	return imgs.length;
 }
 function hrld_showcase_flush(){
-	links = '';
-	imgs_length = '';
 	clicked_id = '';
-
+	return;
 }
 function hrld_showcase_original_url( img_url){
-	return img_url.replace(/(\-{1}\d+x\d+)/, '');
+	var ext = img_url.match(/(jpg|tiff|gif){1}/);
+	return img_url.replace(/(\-{1}\d+x\d+(\-*\d*\.*\w*){1})/, '.'+ext[0]);
 }
 function hrld_showcase_find_clicked_id(img){
 
@@ -69,8 +95,53 @@ function hrld_showcase_find_clicked_id(img){
 			return (c[s].split('showcase-'))[1];
 	}
 }
-function hrld_showcase_build_gallery(){
+function hrld_showcase_build_gallery( container, clicked_id){
+
+	var galleryHTML = nav_wrapper = '';
+	galleryHTML += "<div class='showcase-gallery'>";
 	
+	// close button
+	galleryHTML += "<div class='gallery-close-wrapper'>";
+	galleryHTML += "<button class=gallery-close><a></a></button>";
+	galleryHTML += "</div>";
+
+
+	galleryHTML += "</div>";
+
+	container.append(galleryHTML);
+	return;
+}
+function hrld_showcase_add_imgs( clicked_id){
+	var img_wrapper = '';
+	img_wrapper += "<imgwrapper>";
+	img_wrapper += "<imgContainer>";
+	img_wrapper += img_wrapper_urls(clicked_id);
+	img_wrapper += "</imgContainer>";
+	img_wrapper += "</imgwrapper>";
+	img_wrapper += "<nav>"
+	img_wrapper += "<div class='showcase-previous'><i></i></div>";
+	img_wrapper += "<div class='showcase-next'><i></i></div>";
+	jQuery('.showcase-gallery').append(img_wrapper);
+	return;
+
+}
+function img_wrapper_urls( clicked_id){
+	if( imgs_length == 1)
+		return '<ul><li class=\'showcase-gallery-img-' + '1' + '\'><img src='+ links+' /></li></ul>';
+	else{
+		var imgUrls = '<ul>', counter = 0;
+		for (var i = clicked_id; i < links.length+1; i++) {
+			counter++;
+			imgUrls += '<li class=\'showcase-gallery-img-' + counter + '\'><img src='+ links[i-1]+' /></li>';
+		};
+		for (var j = 1 ; j < clicked_id; j++) {
+			counter++;
+			imgUrls += '<li class=\'showcase-gallery-img-' + counter + '\'><img src='+ links[j-1]+' /></li>';
+		};
+		imgUrls += '</ul>';
+		return imgUrls;
+	}
+
 }
 
 
