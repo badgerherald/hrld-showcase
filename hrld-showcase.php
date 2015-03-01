25a6bcb72eecbb60c66e7773379fa7d521e7dbe9
Matt Neil <mneil@wi.rr.com>
2015-02-28 20:48:53 -0600

    The user/theme should enqueue the scripts and initialization script so there is more freedom in where to activate it

--- hrld-showcase.php
+++ hrld-showcase.php
@@ -15,50 +15,11 @@ function hrld_showcase_enqueue(){
   	//wp_register_script( 'hrld-showcase-script-class', '/wp-content/plugins/hrld-showcase/showcase.js', array( 'jquery' ));
 	//wp_register_script( 'hrld-showcase-script-class', '/wp-content/plugins/hrld-showcase/script-2.js', array( 'jquery' ));
 	wp_register_script( 'hrld-showcase-script-class', '/wp-content/plugins/hrld-showcase/script-3.js', array( 'jquery' ));
-	wp_register_script( 'hrld-showcase-init', '/wp-content/plugins/hrld-showcase/showcase-init.js', array( 'jquery', 'hrld-showcase-script-class' ));
 	wp_localize_script('hrld-showcase-script-class', 'hrld_showcase', array(
 		'ajaxurl' => admin_url('admin-ajax.php'),
 	));
 }
 add_action( 'wp_enqueue_scripts', 'hrld_showcase_enqueue' );
-
-function hrld_showcase_enqueue_single(){
-	if( is_single() ){
-		wp_enqueue_style('hrld-showcase-style');
-		wp_enqueue_script( 'hrld-showcase-script-class');
-		wp_enqueue_script( 'hrld-showcase-init');
-		/* // not sure how this would work if done this way...
-		global $post;
-		$image_urls = array();
-		$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image'));
-		if( !empty($images)){
-			foreach( $images as $id => $image){
-				echo 'id: '.$id;
-				$image_urls['id'.$id] = wp_get_attachment_image_src($id, 'large');
-			}
-		}
-		wp_localize_script( 'hrld-showcase-script', 'photo_links', $image_urls);
-
-		*/ 
-
-		
-
-	}
-} 
-add_action( 'wp_enqueue_scripts', 'hrld_showcase_enqueue_single' );
-
-/*
-function hrld_showcase_enqueue_author(){
-
-	if( is_author()){
-		$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image'));
-	}
-
-
-
-	wp_localize_script( 'hrld-showcase-script', 'photo-links', '');
-} add_action( 'wp_enqueue_scripts', 'hrld_showcase_enqueue_single' );
-*/
 
 function hrld_showcase_add_class_to_thumbnail($thumb, $post_id, $post_thumbnail_id) {
 	if( is_single() ) 
--- showcase-init.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function( jQuery) {
-    /*
-	jQuery('div#page .showcase-block').hrld_showcase({
-		imgs: "img.wp-post-image, img[class*='wp-image-']"//, 
-		//container: "div#content"
-	});
-*/
-    jQuery('div#page .showcase-block').showcase();
-});
